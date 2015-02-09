0d9b38062a50c14536aca696146da20c70be5b92
Vipin SIngh <er.vipinsingh1@gmail.com>
2015-02-09 20:41:05 +0530

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,39 @@
+<div id="footer">
+    <div class="row">
+        <div class="footer-container">
+            <div class="four-top">
+            <h3>Copyright © 2010-2011 srv learning. All rights reserved.</h3>
+            </div>
+            <div class="ftcontent right">
+					<ul class="uicommunity tms" style="display:none;">
+		<li class="Lpbs"><a href="https://www.facebook.com/" target="_blank">
+		<span title="Facebook Page " class="smsprite sm1">&nbsp;</span></a></li>
+		<li class="Lpbs"><a href="https://twitter.com/" target="_blank">
+		<span title="" class="smsprite sm2">&nbsp;</span>
+		</a></li>
+		<li class="Lpbs"><a href="https://in.linkedin.com//34/b58/304" target="_blank">
+		<span title="Official LinkedIn Account of  Systems" class="smsprite sm3">&nbsp;</span>
+		</a></li>
+		<li class="Lpbs"><a href="https://plus.google.com//posts/p/pub" target="_blank">
+		<span title="Google Plus Official page" class="smsprite sm4">&nbsp;</span></a></li>
+		<li class="Lpbs"><a href="http://" target="_blank">
+		<span title="Official Blog" class="smsprite sm5">&nbsp;</span></a></li>
+		</ul> 
+		
+				
+					<ul class="social-icons colored fixclear" >
+				<li class="title">GET SOCIAL</li>
+				<li class="social-facebook"><a href="https://www.facebook.com/" target="_blank">Facebook</a></li>
+				<li class="social-twitter"><a href="https://twitter.com/" target="_blank">Twitter</a></li>
+				<li class="social-linkedin"><a href="https://in.linkedin.com/pub/34/b58/304" target="_blank">Linkedin</a></li>
+				<li class="social-gplus"><a href="https://plus.google.com/107711024052049540802/posts/" target="_blank">Google Plus</a></li>
+				<li class="social-pinterest"><a href="http://www.pinterest.com//" target="_blank">Social Pinterset</a></li>
+				<li class="social-rss"><a href="" target="_blank">Blog</a></li>
+				</ul>
+                
+			</div>
+			
+        </div>
+    </div><!-- end .row-->
+    <div class="clear"></div>
+</div>

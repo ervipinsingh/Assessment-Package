2acbf5c270cc498d0e34cd2e9f545e887a6db289
Vipin SIngh <er.vipinsingh1@gmail.com>
2015-02-09 20:29:22 +0530

    all Library include file

---
+++ libs/all_libs/all_libs.php
@@ -0,0 +1,26 @@
+<?php
+  /* Libs Includes*/
+  require_once(FPATH_BASE.DS.'libs'.DS.'defines.inc');
+  require_once(FPATH_BASE.DS.'include'.DS.'filenames.php' );       
+  require_once(FPATH_BASE.DS.'include'.DS.'site_entry.php');   
+  require_once(FPATH_BASE.DS.'include'.DS.'configuration.php');
+  require_once(FPATH_BASE.DS.'libs'.DS.'css'.DS.'css.inc');
+  require_once(FPATH_BASE.DS.'libs'.DS.'plugins'.DS.'plugin.inc');
+   require_once(FPATH_BASE.DS.'libs'.DS.'sql'.DS.'pb_test_sql.inc');
+  require_once(FPATH_BASE.DS.'include'.DS.'public_test_class.php'); 
+  require_once(FPATH_BASE.DS.'libs'.DS.'sql'.DS.'common.inc');
+  require_once(FPATH_BASE.DS.'include'.DS.'common_functions.php' ); 
+ /* require_once(FPATH_BASE.DS.'libs'.DS.'sql'.DS.'user_sql.inc');
+  require_once(FPATH_BASE.DS.'include'.DS.'user_class.php'); 
+  require_once(FPATH_BASE.DS.'libs'.DS.'sql'.DS.'course_sql.inc');
+  require_once(FPATH_BASE.DS.'include'.DS.'course_class.php');
+   #Question Libs File Inclusion Here
+  require_once(FPATH_BASE.DS.'libs'.DS.'sql'.DS.'qstn_sql.inc'); 
+  require_once(FPATH_BASE.DS.'include'.DS.'public_ass_class.php'); 
+  #Assessment
+  require_once(FPATH_BASE.DS.'libs'.DS.'sql'.DS.'ass_sql.inc'); 
+  require_once(FPATH_BASE.DS.'include'.DS.'ass_class.php'); 
+   #
+ */
+  require_once(FPATH_BASE.DS.'libs'.DS.'js'.DS.'js.inc'); 
+?>

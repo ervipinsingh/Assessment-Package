50a768e4135e3e5c4d688c073373133eba8f1942
Vipin SIngh <er.vipinsingh1@gmail.com>
2015-02-09 20:37:38 +0530

    Create configuration.php

---
+++ includes/configuration.php
@@ -0,0 +1,20 @@
+<?php
+/*Data Base Connectivity Class*/
+ class msConfig
+  { 	 
+	
+	 var $hostnamew='localhost'; 
+	 var $username='root'; 
+	 var $password='pass'; 
+	 var $persist=false;
+	 var $database='web_lms';  
+	
+ /*	
+   var $hostnamew='localhost'; 
+	 var $username='msmainac_weblms'; 
+	 var $password='web@lmsadmin'; 
+	 var $persist=false;
+	 var $database='msmainac_lmsweb';  	       
+	*/
+  }
+?>
